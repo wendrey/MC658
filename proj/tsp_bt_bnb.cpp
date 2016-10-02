9b44fe8ce93dbedb9942064742e7cf18e3cc4afd
wendrey <lordwendrey@gmail.com>
2016-10-02 00:21:07 -0300

    lab01

--- proj/tsp_bt_bnb.cpp
+++ proj/tsp_bt_bnb.cpp
@@ -18,7 +18,8 @@
 #include "mygraphlib.h"
 #include "tsp_bt_bnb.h"
 
-bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit);
+void updateSolution (TSP_Data &tsp, double cost, vector<Node> circuit);
+bool bfs (TSP_Data &tsp, int maxTime, Node u, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit);
 
 //		cerr << "-------------------" << endl;		
 //		cerr << "BFS : " << visit << endl;
@@ -42,42 +43,11 @@ bool bt(TSP_Data &tsp, int maxTime) {
 		node[n] = false;
 			
 	for (ListGraph::NodeIt n(tsp.g); n != INVALID; ++n)
-		return bfs(tsp, maxTime, , n, 0, node, t, circuit);
+		return bfs(tsp, maxTime, n, 0, node, t, circuit);
 	
 }
 
-void updateSolution (TSP_Data &tsp, double cost, vector<Node> circuit) {
-
-	// acha o menor vertice na nova solucao
-
-	int k = 0;
-	string str = tsp.vname[circuit[k]];
-
-	for (int i = 1; i < circuit.size(); i++) {
-		if (strcmp(str,tsp.vname[circuit[i]) > 0)
-			k = i;
-	
-	// verifica se tem melhor solucao
-
-	if (cost == tsp.BestCircuitValue) {	
-		for (int i = 0; i < tsp.NNodes; i++) {
-			int aux = strcmp(tsp.vname[BestCircuit[i]],tsp.vname[circuit[(k+i)%tsp.NNodes]]);
-			if (aux > 0)
-				break;
-			if (aux < 0)				
-				return;
-		}
-	}
-
-	// atualiza melhor solucao
-
-	for (int i = 0; i < tsp.NNodes; i++)
-		tsp.BestCircuit[i] = circuit[(k+i)%tsp.NNodes];
-	tsp.BestCircuitValue = cost;
-								
-}
-
-bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit) {
+bool bfs (TSP_Data &tsp, int maxTime, Node u, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit) {
 
 	// verifica o tempo de execucao
 
@@ -97,12 +67,12 @@ bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMa
 		// se achar uma solucao melhor, atualiza a solucao
 		
 		if (node[v] == false && cost + tsp.weight[e] < tsp.BestCircuitValue)
-			bfs(tsp, maxTime, v, visit+1, cost + tsp.weight[e], node, t, circuit);
+			bfs(tsp, maxTime, v, cost + tsp.weight[e], node, t, circuit);
 		
 		// se achou o ciclo, verifica se a solucao melhora
 		// se achar uma solucao melhor, atualiza a solucao
 		
-		else if (v == circuit.front() && visit+1 == tsp.NNodes) 
+		else if (v == circuit.front() && circuit.size() == tsp.NNodes) 
 			if (cost + tsp.weight[e] <= tsp.BestCircuitValue)
 				updateSolution(tsp, cost + tsp.weight[e], circuit);
 	
@@ -120,6 +90,37 @@ bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMa
 		return true;
 	return false;
 
+}
+
+void updateSolution (TSP_Data &tsp, double cost, vector<Node> circuit) {
+
+	// acha o menor vertice na nova solucao
+
+	int k = 0;
+	string str = tsp.vname[circuit[k]];
+
+	for (int i = 1; i < circuit.size(); i++)
+		if (strcmp(str,tsp.vname[circuit[i]) > 0)
+			k = i;
+	
+	// verifica se tem melhor solucao
+
+	if (cost == tsp.BestCircuitValue) {	
+		for (int i = 0; i < tsp.NNodes; i++) {
+			int aux = strcmp(tsp.vname[tsp.BestCircuit[i]],tsp.vname[circuit[(k+i)%tsp.NNodes]]);
+			if (aux > 0)
+				break;
+			if (aux < 0)				
+				return;
+		}
+	}
+
+	// atualiza melhor solucao
+
+	for (int i = 0; i < tsp.NNodes; i++)
+		tsp.BestCircuit[i] = circuit[(k+i)%tsp.NNodes];
+	tsp.BestCircuitValue = cost;
+								
 }
 
 //------------------------------------------------------------------------------
