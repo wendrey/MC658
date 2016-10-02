e7a5df3b18b114c2e59987ce25362a8950204482
wendrey <lordwendrey@gmail.com>
2016-10-01 22:35:53 -0300

    lab01

--- proj/tsp_bt_bnb.cpp
+++ proj/tsp_bt_bnb.cpp
@@ -20,40 +20,6 @@
 
 bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit);
 
-//------------------------------------------------------------------------------
-
-bool bt(TSP_Data &tsp, int maxTime) {
-
-	clock_t t = clock();
-	vector<Node> circuit;
-
-	NodeBoolMap node(tsp.g);
-	for (ListGraph::NodeIt n(tsp.g); n != INVALID; ++n)
-		node[n] = false;
-	
-	for (ListGraph::NodeIt n(tsp.g); n != INVALID; ++n)
-		return bfs(tsp, maxTime, n, 0, 0, node, t, circuit);
-	
-	return false;
-	
-}
-
-bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit) {
-
-	node[u] = true;
-	circuit.push_back(u);
-		
-	// dado um vertice, passa por todos seus vizinhos	
-
-	for (ListGraph::IncEdgeIt e(tsp.g, u); e != INVALID; ++e) {
-		
-		// verifica o tempo de execucao
-
-		if (maxTime < (clock() - t) / CLOCKS_PER_SEC)
-			return false;
-
-		Node v = tsp.g.target(e);
-		
 //		cerr << "-------------------" << endl;		
 //		cerr << "BFS : " << visit << endl;
 //		cerr << "Node u : " << tsp.g.id(u) << endl;					
@@ -61,14 +27,46 @@ bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMa
 //		cerr << "Arc u->v : " << tsp.g.id(e) << endl;
 //		cerr << "Weight : " << tsp.weight[e] << endl;
 //		cerr << "Map v : " << node[v] << endl;		
-				
+
+//------------------------------------------------------------------------------
+
+bool bt(TSP_Data &tsp, int maxTime) {
+
+	clock_t t = clock();
+	vector<Node> circuit.clear();
+	tsp.g.BestCircuit.clear();
+
+	NodeBoolMap node(tsp.g);
+	for (ListGraph::NodeIt n(tsp.g); n != INVALID; ++n) {
+		node[n] = false;
+		cerr << tsp.vname[n] << endl;
+	}
+		
+	return false; bfs(tsp, maxTime, n, 0, 0, nodeFromId(0), t, circuit);
+	
+}
+
+bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMap &node, clock_t t, vector<Node> circuit) {
+
+	// verifica o tempo de execucao
+
+	if (maxTime < (clock() - t) / CLOCKS_PER_SEC)
+		return false;
+		
+	// poe o vertice na solucao e passa por todos seus vizinhos	
+
+	node[u] = true;
+	circuit.push_back(u);
+
+	for (ListGraph::IncEdgeIt e(tsp.g, u); e != INVALID; ++e) {
+		
+		Node v = tsp.g.target(e);
+						
 		// se existe uma potencial solucao, continua a busca
 		// se achar uma solucao melhor, atualiza a solucao
 		
-		if (node[v] == false && cost + tsp.weight[e] < tsp.BestCircuitValue) {
-			if (bfs(tsp, maxTime, v, visit+1, cost + tsp.weight[e], node, t, circuit))
-				return true;	
-		}
+		if (node[v] == false && cost + tsp.weight[e] < tsp.BestCircuitValue)
+			bfs(tsp, maxTime, v, visit+1, cost + tsp.weight[e], node, t, circuit);
 		
 		// se achou o ciclo, verifica se a solucao melhora
 		// se achar uma solucao melhor, atualiza a solucao
@@ -77,14 +75,21 @@ bool bfs (TSP_Data &tsp, int maxTime, Node u, int visit, double cost, NodeBoolMa
 			if (cost + tsp.weight[e] < tsp.BestCircuitValue) {					
 				tsp.BestCircuitValue = cost + tsp.weight[e];
 				tsp.BestCircuit = circuit;
-				return true;
 			}
 		}					
 	
 	}
 
+	// tira o vertice da solucao
+
 	circuit.pop_back();	
 	node[u] = false;
+	
+	// se viu todas combinacoes posiveis e tem solucao, retorna solucao otima
+	// se viu apenas as combinacoes de um determinado ramo, retorna falso
+	
+	if (circuit.empty && BestCircuit.size == tsp.NNodes)
+		return true;
 	return false;
 
 }
